2f4f88eaec3f57336d4f045f33f49b0875b97be1
Aditya Garg <gargaditya08@live.com>
2021-11-22 13:47:38 +0530

    Mojave

--- files/chroot_build.sh
+++ files/chroot_build.sh
@@ -68,10 +68,10 @@ apt-get install -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="
 
 # This is not ideal, but it should work until the apt repo gets updated.
 
-curl -L https://github.com/AdityaGarg8/T2-Big-Sur-Ubuntu-Kernel/releases/download/v5.15.3-1/linux-headers-5.15.3-t2-big-sur_5.15.3-1_amd64.deb > /tmp/headers-bs.deb
-curl -L https://github.com/AdityaGarg8/T2-Big-Sur-Ubuntu-Kernel/releases/download/v5.15.3-1/linux-image-5.15.3-t2-big-sur_5.15.3-1_amd64.deb > /tmp/image-bs.deb
+curl -L https://github.com/AdityaGarg8/T2-Mojave-Ubuntu-Kernel/releases/download/v5.15.3-1/linux-headers-5.15.3-t2-mojave_5.15.3-1_amd64.deb > /tmp/headers-m.deb
+curl -L https://github.com/AdityaGarg8/T2-Mojave-Ubuntu-Kernel/releases/download/v5.15.3-1/linux-image-5.15.3-t2-mojave_5.15.3-1_amd64.deb > /tmp/image-m.deb
 file /tmp/*
-apt install /tmp/headers-bs.deb /tmp/image-bs.deb
+apt install /tmp/headers-m.deb /tmp/image-m.deb
 
 echo >&2 "===]> Info: Install window manager... "
 
